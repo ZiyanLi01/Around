69bf3b2af849208709bfc45fe20a2c14f04be6a4
Ziyan Li <liziyan1992@gmail.com>
2023-12-11 21:09:59 +0800

    design home page, login, signup page

---
+++ around-web/src/components/Home.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+import SearchBar from "./SearchBar";
+
+function Home(props) {
+ return (
+   <div className="home">
+     <SearchBar />
+   </div>
+ );
+}
+
+export default Home;
---
+++ around-web/src/constants.js
@@ -0,0 +1,8 @@
+export const TOKEN_KEY = 'token';
+export const BASE_URL = '<use your backend url>'; 
+
+export const SEARCH_KEY = {
+    all: 0,
+    keyword: 1,
+    user: 2
+   };
