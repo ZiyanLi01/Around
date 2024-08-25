6479b43c1dba742276d3179d4806d30f4d0dee46
ZiyanLi01 <liziyan1992@gmail.com>
2024-08-25 16:02:17 +0000

    add elastic search function

--- go/src/around/main.go
+++ go/src/around/main.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"around/backend"
 	"around/handler"
 )
 
 func main() {
 	fmt.Println("started-service")
+	backend.InitElasticsearchBackend()
 	log.Fatal(http.ListenAndServe(":8080", handler.InitRouter()))
 }
