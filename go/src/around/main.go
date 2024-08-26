40c0a0d9c5c94588320855b1e773e715f24148c1
ZiyanLi01 <liziyan1992@gmail.com>
2024-08-26 17:34:29 +0000

    reconstruct service layer and make backend finally works

--- go/src/around/main.go
+++ go/src/around/main.go
@@ -8,17 +8,11 @@ import (
 
 	"around/backend"
 	"around/handler"
-	"around/util"
 )
 
 func main() {
-
 	fmt.Println("started-service")
-	config, err := util.LoadApplicationConfig("conf", "deploy.yml")
-	if err != nil {
-		panic(err) 
-	}
-	backend.InitElasticsearchBackend(config.ElasticsearchConfig)
-	backend.InitGCSBackend(config.GCSConfig)
-	log.Fatal(http.ListenAndServe(":8080", handler.InitRouter(config.TokenConfig)))
+	backend.InitElasticsearchBackend()
+	backend.InitGCSBackend()
+	log.Fatal(http.ListenAndServe(":8080", handler.InitRouter()))
 }
